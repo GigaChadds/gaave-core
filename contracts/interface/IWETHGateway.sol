2a64e6add1a2ad0e424b12365849171508220ea5
Kenk <kenghin_lim@hotmail.com>
2022-08-07 16:38:13 +0800

    Add IWETHGateway

---
+++ contracts/interface/IWETHGateway.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.10;
+
+interface IWETHGateway {
+    function depositETH(
+        address lendingPool,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external payable;
+
+    function withdrawETH(
+        address lendingPool,
+        uint256 amount,
+        address onBehalfOf
+    ) external;
+
+    function repayETH(
+        address lendingPool,
+        uint256 amount,
+        uint256 rateMode,
+        address onBehalfOf
+    ) external payable;
+
+    function borrowETH(
+        address lendingPool,
+        uint256 amount,
+        uint256 interesRateMode,
+        uint16 referralCode
+    ) external;
+}
