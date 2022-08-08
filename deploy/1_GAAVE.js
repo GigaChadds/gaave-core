3e41751177fbcebd800754a931f710de24b0e907
Kenk <kenghin_lim@hotmail.com>
2022-08-08 15:10:18 +0800

    Update Token Addresses in deploy script

--- deploy/1_GAAVE.js
+++ deploy/1_GAAVE.js
@@ -13,9 +13,23 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   // Config
   console.log(`Deploying GAAVE... from ${deployer}`);
 
-  let gaave = await deploy("GAAVE", {
+  // Token Addresses
+  let TOKEN_ADDRESSES = [
+    "0x9A753f0F7886C9fbF63cF59D0D4423C5eFaCE95B",
+    "0xb685400156cF3CBE8725958DeAA61436727A30c3",
+  ];
+
+  // Chainlink Addresses
+  let CHAINLINK_ADDRESSES = [
+    "0x0FCAa9c899EC5A91eBc3D5Dd869De833b06fB046", //DAI
+    "0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada", //MATIC
+  ];
+
+  // Mapping
+
+  let gaave = await deploy("GAAVECore", {
     from: deployer,
-    args: [ETH_GATEWAY, POOL_PROXY],
+    args: [ETH_GATEWAY, POOL_PROXY, TOKEN_ADDRESSES, CHAINLINK_ADDRESSES],
   });
 
   gasLogger.addDeployment(gaave);
